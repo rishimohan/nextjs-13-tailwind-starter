f2cc708e02e746224e84839419cd48f9bd3e552d
Rishi <iamrishi.ms@gmail.com>
2023-05-25 17:34:10 +0200

    cleanup

--- src/app/page.js
+++ src/app/page.js
@@ -7,11 +7,11 @@ import clsx from 'clsx'
 export default () => (
   <div className="min-w-screen min-h-screen flex items-center justify-center bg-gradient-to-br from-purple-200 to-purple-400 p-5">
     <div className="bg-gradient-to-br from-white/40 to-white/50 border border-white/50 p-[8px] rounded-[20px] backdrop-blur shadow-[0_20px_280px_rgba(0,0,0,0.2)]">
-      <div className="bg-white rounded-[12px] p-8 md:p-20 shadow-[0_0_12px_rgba(0,0,0,0.2)]">
+      <div className="bg-white rounded-[12px] p-8 md:p-20 shadow-[0_0_12px_rgba(0,0,0,0.2)] max-w-[550px]">
         <h2 className={clsx("text-4xl font-bold mb-3", instrument.className)}>
           Contains →
         </h2>
-        <ul className="list-disc list-inside opacity-70 space-y-1">
+        <ul className="list-disc list-inside text-gray-500 space-y-1">
           <li>Next.js 13</li>
           <li>TailwindCSS</li>
           <li>Radix UI components</li>
@@ -23,15 +23,20 @@ export default () => (
         >
           How to use →
         </h2>
-        <ul className="list-disc list-inside opacity-70 space-y-1">
-          <li>Clone this repo or use as template</li>
+        <ul className="list-disc list-inside text-gray-500 space-y-1">
+          <li>
+            Clone this repo or use as template
+            <span className={clsx(pt_mono.className, "text-black border-b border-gray-200")}>
+              `git clone git@github.com:rishimohan/nextjs-13-tailwind-starter.git`
+            </span>
+          </li>
           <li>
             Get started by editing{" "}
-            <span className={pt_mono.className}>`src/app/page.js`</span>
+            <span className={clsx(pt_mono.className, "text-black border-b border-gray-200")}>`src/app/page.js`</span>
           </li>
           <li>
             All the UI components are at{" "}
-            <span className={pt_mono.className}>`ui/index.js`</span>
+            <span className={clsx(pt_mono.className, "text-black border-b border-gray-200")}>`ui/index.js`</span>
           </li>
         </ul>
       </div>
